5a9b3bf88c58db64a42d8ea7b6a01359125852d4
Nipun-9420 <71821115+Nipun-9420@users.noreply.github.com>
2024-04-11 13:45:10 +0530

    test

--- T_flip_flop/t_flip__flop_tb.v
+++ T_flip_flop/t_flip__flop_tb.v
@@ -1,44 +1,41 @@
 module d_ff_gate_tb;
-    reg d, clk;
-    wire q, qbar;
-    t_ff DUV (q, qbar, d, clk);
+  reg t, clk;
+  wire q, qbar;
+  t_ff DUV(qq,qqbar,t,cclk);
+initial begin
+  $dumpfile("dump.vcd");
+  $dumpvars(1);
+end
   initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars(1);
+    clk=0;
   end
-    initial begin
-      clk=0;
+  always
+    begin
+    clk= ~ clk;#10;
     end
-    always
-      begin
-      clk= ~ clk;#10;
-      end
-    
-    initial begin
-      clk = 0;
-      d = 0;
-      #10 d = 1'b1;
-      #10 d = 1'b0;
-      #10 d = 1'b1;
-      #5 d = 1'b0;
-      #20 d = 1'b0;
-      #17 d = 1'b1;
-      #17 d = 1'b0;
-      #17 d = 1'b1;
-      #17 d = 1'b0;
-      #17 d = 1'b1;
-      #17 d = 1'b1;
-      
-      #17 d = 1'b1;
-      #17 d = 1'b1;
-      
-      #10 d = 1'b0;
-      #10 d = 1'b0;
-      
-      #10 d = 1'b1;
-      #10 d = 1'b1;
-      #100;
   
-      $stop;
-    end
-  endmodule
+  initial begin
+    clk = 0;
+    d = 0;
+    #10 t = 1'b1;
+    #10 t = 1'b1;
+    #10 t = 1'b1;
+    #05 t = 1'b1;
+    #20 t = 1'b1;
+    #17 t = 1'b1;
+    #17 t = 1'b1;
+    #17 t = 1'b1;
+    #17 t = 1'b1;
+    #17 t = 1'b1;
+    #17 t = 1'b1;    
+    #17 t = 1'b1;
+    #17 t = 1'b1;    
+    #10 t = 1'b1;
+    #10 t = 1'b1;
+    #10 t = 1'b1;
+    #10 t = 1'b1;
+    #100;
+
+    $stop;
+  end
+endmodule
